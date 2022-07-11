41f843cce0cf84bb5ed1ecca3d8053272a089bd3
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-10 23:02:55 -0300

    add new solution dont work

--- challenges/25/codigo.js
+++ challenges/25/codigo.js
@@ -1,7 +1,7 @@
 const room = [
     [' ', ' ', ' '],
     [' ', ' ', 'm'],
-    ['*', ' ', ' ']
+    [' ', ' ', ' ']
   ]
 const room1 = [
     [' ', ' ', ' '],
@@ -18,15 +18,17 @@ function canMouseEat(direction, game) {
     // ¡Gracias por jugar a AdventJS 2021! 🤗
     // ¡Nos vemos el año que viene! 👋
     // Por favor, comparte en las redes qué te ha parecido! 🚀
-    for (let i = 0; i < game.length; i++) {
-        direction === 'up' && game[i][(i-1).indexOf('*')] ? true :
-        direction === 'down' && game[i][(i+1).indexOf('*') ] ? true :
-        direction === 'right' && game[i][(i).indexOf('*') + 1] ? true :
-        direction === 'left' && game[i][(i).indexOf('*') -1] ? true : false
+    let result = [];
+    for (let i = 0; i < game.length ; i++) {
+       result.push(direction === 'down' && game[i].indexOf('*') === game[i < game.length -1 ? i+1 : i ].indexOf('m') && game[i].indexOf('m') !== -1 ? true : false
+        /* direction === 'right' && game[i].indexOf('*') === game[i].indexOf('m') + 1 && game[i].indexOf('*') !== -1 ? true :
+        direction === 'left' && game[i].indexOf('*') === game[i].indexOf('m') - 1 && game[i].indexOf('*') !== -1 ? true : 
+        i > 0 && direction === 'up' && game[i].indexOf('*') === game[i-1].indexOf('m') && game[i].indexOf('*') !== -1 ? true : false */)
+        console.log(result)
     }
- 
+    return result.includes(true)
   }
-/* canMouseEat('up',    room)
+/*canMouseEat('up',    room)
 canMouseEat('down',  room)
 canMouseEat('right', room)
 canMouseEat('left',  room)
