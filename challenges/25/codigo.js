f932e458610b2537e1366acce359556aee172285
lucas-rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-24 15:04:51 -0300

    add new solution

--- challenges/25/codigo.js
+++ challenges/25/codigo.js
@@ -20,11 +20,11 @@ function canMouseEat(direction, game) {
     // Por favor, comparte en las redes qué te ha parecido! 🚀
     let result = [];
     for (let i = 0; i < game.length ; i++) {
-       result.push(direction === 'down' && game[i].indexOf('*') === game[i < game.length -1 ? i+1 : i ].indexOf('m') && game[i].indexOf('m') !== -1 ? true : false
-        /* direction === 'right' && game[i].indexOf('*') === game[i].indexOf('m') + 1 && game[i].indexOf('*') !== -1 ? true :
-        direction === 'left' && game[i].indexOf('*') === game[i].indexOf('m') - 1 && game[i].indexOf('*') !== -1 ? true : 
-        i > 0 && direction === 'up' && game[i].indexOf('*') === game[i-1].indexOf('m') && game[i].indexOf('*') !== -1 ? true : false */)
-        console.log(result)
+       result.push(direction === 'down' && game[i].indexOf('*') === game[i < game.length -1 ? i+1 : i ].indexOf('m') && game[i].indexOf('m') !== -1 ? true : 
+        direction === 'right' && game[i].indexOf('m') + 1 === game[i].indexOf('*')  && game[i].indexOf('*') !== -1 && game[i].indexOf('m') !== -1 ? true :
+        direction === 'left' && game[i].indexOf('m') - 1 === game[i].indexOf('*') && game[i].indexOf('*') !== -1 ? true : 
+        i > 0 && direction === 'up' && game[i].indexOf('*') === game[i-1].indexOf('m') && game[i].indexOf('*') !== -1 ? true : false)
+        //console.log(result)
     }
     return result.includes(true)
   }
